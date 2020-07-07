bd9e6df8dcc678553881685920150de92519a0da
Toni <sahmir.toni66@gmail.com>
2020-07-08 00:47:10 +0700

    membuat functions & tampil data

--- kuliah/pertemuan9/pw_2020.sql
+++ kuliah/pertemuan9/pw_2020.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 07, 2020 at 12:54 AM
+-- Generation Time: Jul 07, 2020 at 03:28 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
@@ -32,8 +32,18 @@ CREATE TABLE `mahasiswa` (
   `id` int(11) NOT NULL,
   `nrp` varchar(100) NOT NULL,
   `nama` varchar(100) NOT NULL,
-  `alamat` varchar(100) NOT NULL
+  `email` varchar(100) NOT NULL,
+  `jurusan` varchar(100) NOT NULL,
+  `gambar` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nrp`, `nama`, `email`, `jurusan`, `gambar`) VALUES
+(1, '098456876', 'Toni', 'toni865@unsub.com', 'Ilmu Komputer', 'cs.png'),
+(2, '345876235', 'Sipen', 'sipen00@gmail.com', 'Imu Komputer', 'digit.png');
 
 --
 -- Indexes for dumped tables
@@ -53,7 +63,7 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
